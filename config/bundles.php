35902202e6218ae0f55cd9b474ead7b6ba5b879f
Dave Redfern <david.redfern@madagasgar.com>
2021-10-01 11:26:32 -0400

    Update packages

--- config/bundles.php
+++ config/bundles.php
@@ -2,12 +2,12 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    SamJ\FractalBundle\SamJFractalBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'docker' => true],
     Somnambulist\Bundles\ApiBundle\SomnambulistApiBundle::class => ['all' => true],
+    Somnambulist\Bundles\FractalBundle\SomnambulistFractalBundle::class => ['all' => true],
     App\Resources\ResourcesBundle::class => ['all' => true],
 ];
