d7a9873258eeb45c5ca983eafd18c83924cdfdc4
Dave Redfern <david.redfern@madagasgar.com>
2022-03-02 09:44:13 -0500

    Update config

--- config/bundles.php
+++ config/bundles.php
@@ -6,8 +6,9 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'docker' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Somnambulist\Bundles\ApiBundle\SomnambulistApiBundle::class => ['all' => true],
     Somnambulist\Bundles\FractalBundle\SomnambulistFractalBundle::class => ['all' => true],
+    Somnambulist\Bundles\FormRequestBundle\SomnambulistFormRequestBundle::class => ['all' => true],
     App\Resources\ResourcesBundle::class => ['all' => true],
 ];
